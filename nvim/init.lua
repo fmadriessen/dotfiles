5304572ca94d6954cb80af4909eaccb63e5d4577
Frank Driessen <8720705+fmadriessen@users.noreply.github.com>
2024-12-28 14:02:43 +0100

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,15 @@
 vim.opt.breakindent = true
-vim.opt.breakindentopt = { list = -1 }
+vim.opt.breakindentopt = "list:-1"
 vim.opt.completeopt = { "menuone", "noinsert", "popup" }
 vim.opt.expandtab = true
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldmethod = "expr"
 vim.opt.foldtext = ""
 vim.opt.ignorecase = true
+vim.opt.shada:append({
+   "r/tmp/",
+   "rterm:",
+})
 vim.opt.shiftwidth = 0
 vim.opt.smartcase = true
 vim.opt.splitbelow = true
@@ -14,8 +18,11 @@ vim.opt.tabstop = 4
 vim.opt.title = true
 vim.opt.undofile = true
 vim.opt.wrap = false
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.loader.enable(true)
 
 vim.env.VIMPLUGINDIR = vim.fn.stdpath("data") .. "/site/pack/deps"
 local deps_path = vim.env.VIMPLUGINDIR .. "/start/mini.deps"
@@ -23,7 +30,6 @@ if not vim.uv.fs_stat(deps_path) then
    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/echasnovski/mini.nvim", deps_path })
    vim.cmd("packadd mini.deps | helptags ALL")
 end
-
 require("mini.deps").setup({ path = { package = vim.fn.stdpath("data") .. "/site/" } })
 
 MiniDeps.add({
@@ -45,6 +51,7 @@ MiniDeps.add({
    },
 })
 MiniDeps.add("stevearc/conform.nvim")
+MiniDeps.add("lewis6991/gitsigns.nvim")
 
 require("kanagawa").setup({
    background = {
@@ -62,14 +69,7 @@ require("kanagawa").setup({
 })
 vim.cmd.colorscheme("kanagawa")
 
-require("blink.cmp").setup({
-   keymap = {
-      preset = "default",
-      ["<C-n>"] = { "show", "select_next", "fallback" },
-      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
-      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-   },
-})
+require("blink.cmp").setup()
 
 require("nvim-treesitter").setup({
    ensure_installed = {
@@ -86,7 +86,6 @@ require("nvim-treesitter").setup({
       "yaml",
    },
 })
-require("nvim-treesitter-textobjects").setup()
 
 vim.api.nvim_create_autocmd("FileType", {
    callback = function(event)
@@ -99,12 +98,15 @@ vim.api.nvim_create_autocmd("FileType", {
    group = vim.api.nvim_create_augroup("TreesitterHighlighting", { clear = true }),
 })
 
+require("gitsigns").setup()
+
 require("conform").setup({
    formatters_by_ft = {
       fish = { "fish_indent" },
       lua = { "stylua", lsp_format = "fallback" },
       markdown = { "prettier", "injected" },
       rust = { "rustfmt", lsp_format = "fallback" },
+      ["_"] = { "trim_whitespace", "trim_newlines" },
    },
    format_on_save = function(buf)
       -- INFO: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md
@@ -134,10 +136,14 @@ local function textobject_swap(direction, capture_group)
    return function() require("nvim-treesitter-textobjects.swap")["swap_" .. direction](capture_group) end
 end
 
+vim.keymap.set({ "x", "o" }, "aa", textobject_select("@parameter.outer"), { desc = "Select around paremeter" })
+vim.keymap.set({ "x", "o" }, "ia", textobject_select("@parameter.inner"), { desc = "Select inside parameter" })
+vim.keymap.set({ "x", "o" }, "ac", textobject_select("@comment.outer"), { desc = "Select around comment" })
+vim.keymap.set({ "x", "o" }, "ic", textobject_select("@comment.inner"), { desc = "Select inside comment" })
+vim.keymap.set({ "x", "o" }, "at", textobject_select("@class.outer"), { desc = "Select class or type" })
+vim.keymap.set({ "x", "o" }, "it", textobject_select("@class.inner"), { desc = "Select class or type" })
 vim.keymap.set({ "x", "o" }, "af", textobject_select("@function.outer"), { desc = "Select around function" })
 vim.keymap.set({ "x", "o" }, "if", textobject_select("@function.inner"), { desc = "Select inside function" })
-vim.keymap.set({ "x", "o" }, "ia", textobject_select("@parameter.inner"), { desc = "Select inside parameter" })
-vim.keymap.set({ "x", "o" }, "aa", textobject_select("@parameter.outer"), { desc = "Select around paremeter" })
 
 vim.keymap.set("n", ">a", textobject_swap("next", "@parameter.inner"), { desc = "Swap parameter forward" })
 vim.keymap.set("n", "<a", textobject_swap("previous", "@parameter.inner"), { desc = "Swap parameter backward" })
@@ -145,10 +151,11 @@ vim.keymap.set("n", "<a", textobject_swap("previous", "@parameter.inner"), { des
 vim.keymap.set("x", ">", ">gv", { desc = "Shift lines inward" })
 vim.keymap.set("x", "<", "<gv", { desc = "Shift lines outward" })
 
-vim.keymap.set({ "i", "s" }, "<C-f>", function()
+-- INFO: as of #27339 nvim binds <Tab> and <S-Tab> to jump between placeholders
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
    if vim.snippet.active({ direction = 1 }) then return vim.snippet.jump(1) end
 end, { desc = "Jump to next placeholder", expr = true })
-vim.keymap.set({ "i", "s" }, "<C-b>", function()
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
    if vim.snippet.active({ direction = -1 }) then return vim.snippet.jump(-1) end
 end, { desc = "Jump to previous placeholder", expr = true })
 vim.keymap.set({ "i", "s" }, "<ESC>", function()
@@ -156,15 +163,38 @@ vim.keymap.set({ "i", "s" }, "<ESC>", function()
    return "<ESC>"
 end, { expr = true })
 
+---List the window handles of floating windows on the given tab page
+---@param tabpage integer? Handle of the tabpage (defaults to current tab)
+---@return integer[]: List of floating windows, empty if none exist
+local function list_floating_windows(tabpage)
+   local windows = vim.api.nvim_tabpage_list_wins(tabpage or 0)
+   local floating_windows = {}
+   for _, window in pairs(windows) do
+      if vim.api.nvim_win_get_config(window).relative then table.insert(floating_windows, window) end
+   end
+   return floating_windows
+end
+
 vim.keymap.set("n", "<C-l>", function()
-   vim.cmd.fclose()
+   for window in list_floating_windows() do
+      if window ~= vim.api.nvim_get_current_win() then vim.api.nvim_win_close(window, false) end
+   end
+
    vim.cmd.nohlsearch()
    vim.cmd.redraw()
 end, { desc = "Close floats and clear search highlights" })
 
+vim.keymap.set({ "n", "x" }, "n", "'Nn'[v:searchforward].'zv'", { desc = "Search forwards", expr = true })
+vim.keymap.set({ "n", "x" }, "N", "'nN'[v:searchforward].'zv'", { desc = "Search backwards", expr = true })
+
 vim.keymap.set("n", "gQ", "<cmd>Format<cr>", { desc = "Format buffer" })
+
 vim.keymap.set("n", "<C-p>", function() require("fzf-lua").files() end, { desc = "Find files" })
+vim.keymap.set("n", "<leader>f", function() require("fzf-lua").files() end, { desc = "Find files" })
 vim.keymap.set("n", "<leader>b", function() require("fzf-lua").buffers() end, { desc = "Show buffers" })
+vim.keymap.set("n", "<leader>/", function() require("fzf-lua").live_grep() end, { desc = "Grep in files" })
+vim.keymap.set("n", "<leader>h", function() require("fzf-lua").helptags() end, { desc = "Find help tags" })
+vim.keymap.set("n", "<leader><leader>", function() require("fzf-lua").resume() end, { desc = "Resume previous search" })
 
 vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, { desc = "Show diagnostics on line" })
 
@@ -182,6 +212,10 @@ vim.keymap.set("n", "<leader>xh", function()
       vim.log.levels.INFO
    )
 end, { desc = "Toggle inlay hints" })
+vim.keymap.set("n", "<leader>xs", function()
+   vim.o.spell = not vim.o.spell
+   vim.notify(string.format("%s spell checking", vim.o.spell and "Enabled" or "Disabled"), vim.log.levels.INFO)
+end, { desc = "Toggle spell checking" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
    pattern = "*",
@@ -238,8 +272,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
       -- =vim.lsp.get_clients()[1].server_capabilities
       -- See also :h lsp-buf
       if client:supports_method("textDocument/foldingRange") then
-         vim.wo.foldmethod = "expr"
-         vim.wo.foldexpr = "v:lua.vim.lsp.foldexpr()"
+         vim.opt_local.foldmethod = "expr"
+         vim.opt_local.foldexpr = "v:lua.vim.lsp.foldexpr()"
       end
    end,
 })
@@ -250,7 +284,9 @@ vim.api.nvim_create_autocmd("FileType", {
    group = augroup_filetype_config,
    callback = function()
       vim.opt_local.conceallevel = 2
+      vim.opt_local.linebreak = true
       vim.opt_local.shiftwidth = 2
       vim.opt_local.tabstop = 2
+      vim.opt_local.wrap = true
    end,
 })
